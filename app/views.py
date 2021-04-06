58943e78a4dd19f208dec723aec0eb6c94107ee2
Ekasy <mikkycat15@gmail.com>
2021-04-06 11:53:30 +0300

    dz2

---
+++ app/views.py
@@ -0,0 +1,103 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator
+
+
+tags = [
+    {
+        'id': idx,
+        'name': f'tag{idx}',
+    } for idx in range(2)
+]
+
+
+questions = [
+    {
+        'id': idx,
+        'title': f'Title number {idx}',
+        'text': f'Some text for question #{idx}',
+        'tags': tags,
+    } for idx in range(10)
+]
+
+
+popular_tags = [
+    {'id': 1, 'name': 'python'}, {'id': 2, 'name': 'perl'}, {'id': 3, 'name': 'technopark'},
+    {'id': 4, 'name': 'cpp'}, {'id': 5, 'name': 'mysql'}, {'id': 6, 'name': 'golang'},
+]
+
+
+def paginate(objects_list, request, per_page=3):
+    p = Paginator(objects_list, per_page)
+    page = request.GET.get('page')
+    objects_by_page = p.get_page(page)
+    return objects_by_page
+
+
+def index(request):
+    questions_by_page = paginate(questions, request)
+    return render(request, 'index.html', {
+        'questions': questions_by_page,
+        'popular_tags': popular_tags,
+    })
+
+
+def hot_questions(request):
+    questions_by_page = paginate(questions, request)
+    return render(request, 'hot_questions.html', {
+        'questions': questions_by_page,
+        'popular_tags': popular_tags,
+    })
+
+
+def new_question(request):
+    return render(request, 'ask.html', {'popular_tags': popular_tags, })
+
+
+def one_question(request, pk):
+    question = questions[pk]
+    comments = [
+        {
+            'id': idx,
+            'text': f"It's probably a correct answer. Result is {idx}",
+        } for idx in range(4)
+    ]
+
+    comments_by_page = paginate(comments, request)
+
+    return render(request, 'question.html', {
+        "question": question,
+        "questions": comments_by_page,  # важно! передаются комменты
+        'popular_tags': popular_tags,
+    })
+
+
+# разобраться
+def questions_by_tag(request, name):
+    que = paginate([questions[i] for i in range(len(name))], request)
+    tag = {"id": len(name), "name": name}
+    return render(request, 'questions_by_tag.html', {
+        "questions": que,
+        "tag": tag,
+        'popular_tags': popular_tags,
+    })
+
+
+def login(request):
+    return render(request, 'login.html', {'popular_tags': popular_tags, })
+
+
+def signup(request):
+    return render(request, 'signup.html', {'popular_tags': popular_tags, })
+
+
+def personal_page(request):
+    personal_info = {
+        "name": "Edin Kasupovich",
+        "login": "ekasy",
+        "nickname": "EKASY",
+        "email": "ekasy@tech.com"
+    }
+    return render(request, 'personal_page.html', {
+        "personal_info": personal_info,
+        'popular_tags': popular_tags,
+    })
