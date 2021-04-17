32b0bd2f0291fb684e2449fc6d90101285ea527c
Ekasy <mikkycat15@gmail.com>
2021-04-17 16:41:37 +0300

    dz2: define all refernces

--- app/views.py
+++ app/views.py
@@ -16,6 +16,8 @@ questions = [
         'title': f'Title number {idx}',
         'text': f'Some text for question #{idx}',
         'tags': tags,
+        'mark': 3,
+        'answer_count': 4,
     } for idx in range(10)
 ]
 
@@ -24,6 +26,9 @@ popular_tags = [
     {'id': 1, 'name': 'python'}, {'id': 2, 'name': 'perl'}, {'id': 3, 'name': 'technopark'},
     {'id': 4, 'name': 'cpp'}, {'id': 5, 'name': 'mysql'}, {'id': 6, 'name': 'golang'},
 ]
+
+
+best_members = ['Mr. Freedman', 'Dr. House', 'Queen Victoria', 'Ekasy']
 
 
 def paginate(objects_list, request, per_page=3):
@@ -38,6 +43,7 @@ def index(request):
     return render(request, 'index.html', {
         'questions': questions_by_page,
         'popular_tags': popular_tags,
+        'best_members': best_members,
     })
 
 
@@ -46,11 +52,12 @@ def hot_questions(request):
     return render(request, 'hot_questions.html', {
         'questions': questions_by_page,
         'popular_tags': popular_tags,
+        'best_members': best_members,
     })
 
 
 def new_question(request):
-    return render(request, 'ask.html', {'popular_tags': popular_tags, })
+    return render(request, 'ask.html', {'popular_tags': popular_tags, 'best_members': best_members, })
 
 
 def one_question(request, pk):
@@ -59,6 +66,7 @@ def one_question(request, pk):
         {
             'id': idx,
             'text': f"It's probably a correct answer. Result is {idx}",
+            'mark': 7,
         } for idx in range(4)
     ]
 
@@ -68,10 +76,10 @@ def one_question(request, pk):
         "question": question,
         "questions": comments_by_page,  # важно! передаются комменты
         'popular_tags': popular_tags,
+        'best_members': best_members,
     })
 
 
-# разобраться
 def questions_by_tag(request, name):
     que = paginate([questions[i] for i in range(len(name))], request)
     tag = {"id": len(name), "name": name}
@@ -79,15 +87,16 @@ def questions_by_tag(request, name):
         "questions": que,
         "tag": tag,
         'popular_tags': popular_tags,
+        'best_members': best_members,
     })
 
 
 def login(request):
-    return render(request, 'login.html', {'popular_tags': popular_tags, })
+    return render(request, 'login.html', {'popular_tags': popular_tags, 'best_members': best_members, })
 
 
 def signup(request):
-    return render(request, 'signup.html', {'popular_tags': popular_tags, })
+    return render(request, 'signup.html', {'popular_tags': popular_tags, 'best_members': best_members, })
 
 
 def personal_page(request):
@@ -100,4 +109,5 @@ def personal_page(request):
     return render(request, 'personal_page.html', {
         "personal_info": personal_info,
         'popular_tags': popular_tags,
+        'best_members': best_members,
     })
